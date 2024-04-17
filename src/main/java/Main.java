fe01e2a78464e5df846d7955006be4dcec417ad3
PiotrNogaj <161605894+PiotrNogaj@users.noreply.github.com>
2024-04-17 13:18:38 +0000

    zadanie35

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                if (i == 0 || i == 4 || j == 0 || j == 4) {
+                if (j == 4 || j == 4 - i || i == 4) {
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");
