9b79efc8ce3ef3656c781356d61eacd5ef920e13
PiotrNogaj <161605894+PiotrNogaj@users.noreply.github.com>
2024-04-17 12:34:11 +0000

    zadanie33

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,17 @@
 // import org.junit.jupiter.api.Test;
 
 public class Main {
-  public static void main(String[] args) {
-    for (int i = 0; i < 5; i++) {
-      for (int j = 0; j < 5; j++) {
+public static void main(String[] args) {
+  for (int i = 4; i >= 0; i--) {
+      for (int j = 0; j <= i; j++) {
           System.out.print("* ");
       }
       System.out.println();
   }
-  }
+}
+}
 
   // @Test
   // void addition() {
   //     assertEquals(2, 1 + 1);
   // }
-}
