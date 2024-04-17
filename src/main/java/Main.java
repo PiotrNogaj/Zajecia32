a5cd299ef49d302482e301b462e4a694ff2dc503
PiotrNogaj <161605894+PiotrNogaj@users.noreply.github.com>
2024-04-17 12:38:21 +0000

    zadanie31

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,14 @@
 // import org.junit.jupiter.api.Test;
 
 public class Main {
-public static void main(String[] args) {
-  for (int i = 4; i >= 0; i--) {
-      for (int j = 0; j <= i; j++) {
-          System.out.print("* ");
-      }
-      System.out.println();
-  }
-}
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
 
   // @Test
