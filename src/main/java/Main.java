7ae4107759bf30663fb93f22ceb91a9ec5340673
PiotrNogaj <161605894+PiotrNogaj@users.noreply.github.com>
2024-04-17 12:19:54 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    for (int i = 0; i < 5; i++) {
+      for (int j = 0; j < 5; j++) {
+          System.out.print("* ");
+      }
+      System.out.println();
+  }
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
